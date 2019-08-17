13c8f5e7e9988f84d1886c3774b30dca4138fef2
jiratkhan <59160471@go.buu.ac.th>
2019-08-17 10:12:53 +0700

    add ID

--- poke.js
+++ poke.js
@@ -4,19 +4,43 @@ const port = 3000
 
 app.use(express.json())
 
+class Pokemon{
+    constructor(name, type){
+        this.name = name
+        this.type = type
+        this.id = null
+    }
+}
+
 app.get('/', (req,res) => res.send(`Hello World!`))
 
-//pokemon -> list all pokemons http//localhost:3000/pokemons
+//pokemons -> list all Pokemons http//localhost:3000/Pokemons
 
-let pokemon = [
-    {name: 'Pikachu', type: 'Electric'},
-    {name: 'Eevee', type: 'Normal'}
-]
-app.get('/pokemons', (req,res) => res.send(pokemon))
+let pokemons = []
 
-app.post('/pokemons', (req,res) => {
-    pokemon.push(req.body)
+    pokemons.push(createPokemon("Pikachu","Electric"))
+    pokemons.push(createPokemon("Eevee","Normal"))
+
+
+function generatenewId(num){
+    let newId = num + 1
+    return newId
+}
+function createPokemon(name,type){
+    let p = new Pokemon(name,type)
+    p.id = generatenewId(pokemons.length)
+    return p
+
+}
+app.get('/Pokemons', (req,res) => res.send(pokemons))
+
+app.post('/Pokemons', (req,res) => {
+    let p = new Pokemon(req.body.name, req.body.type)
+    p.id = generatenewId(pokemons.length)
+
+    pokemons.push(p)
     res.sendStatus(201)
 })
 
 app.listen(port, () => console.log(`Pokemon API lidten on port ${port}`))
+
